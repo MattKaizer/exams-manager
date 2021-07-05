bfb5f013848056689f9c293f3e69a86a8d9be5cc
Matias Benjamín Moreno Juárez <matt@Imac-de-Matias.home>
2021-07-05 14:34:49 +0100

    eureka server

---
+++ microservices-eureka/src/main/java/com/mbm/microservices/eureka/app/MicroservicesEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.mbm.microservices.eureka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroservicesEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesEurekaApplication.class, args);
+	}
+
+}
